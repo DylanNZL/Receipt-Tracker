c10140a635a849ce207a41a337e4a929bac28721
Dylan Cross <dylancross@me.com>
2018-03-01 21:35:00 +1300

    DB receipt_details functions

--- database/receipt_details.js
+++ database/receipt_details.js
@@ -1,3 +1,68 @@
-const bookshelf = require("./bookshelf.js");
+const bookshelf = require("../bookshelf.js");
 
-const mReceiptDetails = require('./models/receipt_details.js');
+const mReceiptDetails = require('../models/receipt_details.js');
+
+async function createReceiptDetail(newReceipt) {
+    return new Promise((resolve, reject) => {
+        mReceiptDetails.forge(newReceipt).save().then(function (data) {
+            console.log(data.attributes);
+            resolve(data.attributes);
+        }).catch(function (err) {
+            console.error('createReceipt error: ' + err);
+            resolve(0);
+        })
+    })
+}
+
+async function testCreateReceiptDetail() {
+    let newReceipt = {
+        receipt_id: 1,
+        quantity: 12,
+        item: "Test Item",
+        price: 12.34
+    };
+
+    let result = await createReceiptDetail(newReceipt);
+
+    console.log(result);
+}
+
+async function deleteReceiptDetail(receiptID) {
+    return new Promise((resolve, reject) => {
+        new mReceiptDetails({ id: receiptID }).destroy().then(function (data) {
+            console.log(data);
+            resolve(true);
+        }).catch(function (err) {
+            console.error('deleteReceipt error: ' + err);
+            resolve(false);
+        })
+    })
+}
+
+async function testDeleteReceiptDetail() {
+    let result = await deleteReceiptDetail(1);
+
+    console.log(result);
+}
+
+async function findReceiptDetailsFromReceipt(receiptID) {
+    return new Promise((resolve, reject) => {
+        new mReceiptDetails({ receipt_id: receiptID }).fetchAll().then(function (data) {
+            console.log(data.models);
+            resolve(data.models);
+        }).catch(function (err) {
+            console.error('deleteReceipt error: ' + err);
+            resolve(false);
+        })
+    })
+}
+
+async function testFindReceiptDetailsFromReceipt() {
+    let result = await findReceiptDetailsFromReceipt(1);
+
+    console.log(result);
+}
+
+exports.createReceiptDetail = createReceiptDetail;
+exports.deleteReceiptDetail = deleteReceiptDetail;
+exports.findReceiptDetailsFromReceipt = findReceiptDetailsFromReceipt;
