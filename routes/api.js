b0f05b18e59fbaf528a5a384f98fba3dd08f2e15
Dylan Cross <dylnnzl@gmail.com>
2018-03-05 17:48:05 +1300

    Added new functions to router

--- routes/api.js
+++ routes/api.js
@@ -9,15 +9,38 @@ const cReceiptDetails = require('../controllers/receipt_details.js');
 router.use('/', function(req, res, next) {
     const url = req.baseUrl;
     switch (url) {
-        case "/api/createAccount":
+        // Account API
+        case "/api/account/create":
             cAccount.createAccount(req, res, next);
             break;
-        case "/api/checkEmailIsUnused":
+        case "/api/account/checkEmail":
             cAccount.checkEmailIsUnused(req, res, next);
             break;
+        // Receipt API
+        case "/api/receipt/create":
+            cReceipt.createReceipt(req, res, next);
+            break;
+        case "/api/receipt/delete":
+            cReceipt.deleteReceipt(req, res, next);
+            break;
+        case "/api/receipt/fromAccount":
+            cReceipt.allReceiptsOfAnAccount(req, res, next);
+            break;
+        // Receipt Details API
+        case "/api/receiptDetails/create":
+            cReceiptDetails.createReceiptDetail(req, res, next);
+            break;
+        case "/api/receiptDetails/create":
+            cReceiptDetails.deleteReceiptDetail(req, res, next);
+            break;
+        case "/api/receiptDetails/create":
+            cReceiptDetails.findDetailsFromReceipt(req, res, next);
+            break;
+        // API Details page
         case "/api/home":
             res.send('api', {});
             break;
+        // 404 Not Found
         default:
             res.status(404).jsonp({
                 error: "API function not found",
